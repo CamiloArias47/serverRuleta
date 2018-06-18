c772f2ac0e5d30d92f8f5ea9f3c5899a138db6e5
CamiloArias47 <camillo47@gmail.com>
2018-06-18 06:35:30 -0500

    Servidor updates

--- servidorRuleta/src/servidorRuleta/Server.java
+++ servidorRuleta/src/servidorRuleta/Server.java
@@ -1,25 +1,159 @@
 package servidorRuleta;
 
+import java.awt.BorderLayout;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class Server {
-	
-	private ServerSocket servidor; 
-	
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+
+import com.sun.glass.events.MouseEvent;
+
+public class Server extends JFrame{
+
+	private ServerSocket servidor;
+	private Socket conexion;
+	private ExecutorService ejecutarJuego; //Facilita la gestión de los hilos jugador
+	private JTextArea areaSalida;
+	private JPanel contenedorSur;
+	private JButton parar;
+
 	public Server() {
+		Gui();
+		createHost();
+	}
+	
+	public void Gui() {
+		areaSalida = new JTextArea(); // crea objeto JTextArea para mostrar la salida
+		areaSalida.setEditable(false);
+		add( areaSalida, BorderLayout.CENTER );
+		areaSalida.setText( "Servidor esperando conexiones\n" );
+		contenedorSur = new JPanel();
+		
+		Escucha evento = new Escucha();
+		parar = new JButton("Parar servidor");
+		parar.addMouseListener(evento);
+		contenedorSur.add(parar);
+		
+		add(contenedorSur, BorderLayout.SOUTH);
+		
+		setSize( 300, 300 ); // establece el tamaño de la ventana
+		setVisible( true ); // muestra la ventana
+	}
+	
+	public void createHost() {
 		// establece el servidor
 		try{
 			System.out.println("[server] Abriendo puerto");
+			mostrarMensaje("[server] Abriendo puerto\n");
 			servidor = new ServerSocket( 12345, 2 );
+			
 		} // fin de try
 		catch ( IOException excepcionES ){
 			excepcionES.printStackTrace();
 			System.out.println("[server] Error: Puerto no abierto");
-			System.exit(1); //cierra la aplicacion
+			mostrarMensaje("[server] Error: Puerto no abierto\n");
+			//System.exit(1); //cierra la aplicacion
 		} // fin de catch
-		
+
 		System.out.println("[server] Puerto abierto");
+		mostrarMensaje("[server] Puerto abierto\n");
+		conect();
+		
+		//ejecutarJuego = Executors.newFixedThreadPool(1); // crea el administrador de los hilos jugador
+		mostrarMensaje("[server] fin del metodo CreateHost\n");
+	}
+
+	
+	public void conect() {
+		mostrarMensaje("[server] Intentando conectar\n");
+		try {
+			conexion = servidor.accept(); 
+			mostrarMensaje("[server] Esperando conexion\n");
+		} // fin de try
+		catch ( IOException excepcionES ) {
+			mostrarMensaje("[server] Error en conexion\n");
+			excepcionES.printStackTrace();
+			//System.exit(1);
+		} // fin de catch
+		//disconnect();
+		mostrarMensaje("[server] ... \n");
 	}
 	
+	public void disconnect() {
+		try {
+			conexion.close();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void closeServerSocket() {
+		mostrarMensaje("[server] cerrando serverSocket\n");
+		try {
+			servidor.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			mostrarMensaje("[server] Errorn al cerrar serverSocket\n");
+			e.printStackTrace();
+		}
+	}
+	
+	private void mostrarMensaje( final String mensajeAMostrar ){
+		SwingUtilities.invokeLater( new Runnable() {
+										 public void run() {
+											 areaSalida.append( mensajeAMostrar ); // agrega el mensaje
+										 } // fin del metodo run
+									   } // fin de la clase interna
+								  ); // fin de la llamada a SwingUtilities.invokeLater
+	} // fin del metodo mostrarMensaje
+	
+	private class EscuchaMouse extends MouseAdapter{
+		public void mouseClicked( MouseEvent e) {
+			mostrarMensaje("[server] Boton clikeado\n");
+			closeServerSocket();
+		}
+	}
+	
+	private class Escucha implements MouseListener{
+		@Override
+		public void mouseClicked(java.awt.event.MouseEvent arg0) {
+			mostrarMensaje("[server] Boton clikeado\n");
+			closeServerSocket();
+		}
+
+		@Override
+		public void mouseEntered(java.awt.event.MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(java.awt.event.MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(java.awt.event.MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(java.awt.event.MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
 }
--- servidorRuleta/src/servidorRuleta/ServerMain.java
+++ servidorRuleta/src/servidorRuleta/ServerMain.java
@@ -1,11 +1,13 @@
 package servidorRuleta;
 
+import javax.swing.JFrame;
 
 public class ServerMain {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Server servidor = new Server();
+		servidor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
